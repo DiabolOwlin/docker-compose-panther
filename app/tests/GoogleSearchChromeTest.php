7ab2d701575985738beb0cbbe23cb0600e2f889b
Valerii Pukas <valerii.pukas@lamia.fi>
2023-06-20 13:50:09 +0200

    Add: working test for Google search introduced

--- app/tests/GoogleSearchChromeTest.php
+++ app/tests/GoogleSearchChromeTest.php
@@ -1,9 +1,11 @@
 <?php
 
+use Facebook\WebDriver\WebDriverDimension;
 use Facebook\WebDriver\WebDriverKeys;
-use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Panther\PantherTestCase;
 use Symfony\Component\Panther\Client;
+use Symfony\Component\Panther\DomCrawler\Crawler;
+
 
 class GoogleSearchChromeTest extends PantherTestCase
 {
@@ -11,14 +13,26 @@ class GoogleSearchChromeTest extends PantherTestCase
     public function testGoogleSearch()
     {
         $client = Client::createFirefoxClient();
+        $size = new WebDriverDimension(1400, 1400);
+        $client->manage()->window()->maximize()->setSize($size);
         $crawler = $client->request('GET', 'https://www.google.com/');
-        $client->waitFor('.gLFyf'); // Wait for up to 10 seconds
+        $button = $crawler->filter('#W0wltc');
+        $button->click();
+        $client->waitFor('.gLFyf');
         $searchInput = $crawler->filter('.gLFyf');
-        $searchInput->sendKeys('test query');
+        $searchInput->sendKeys('marcus rashford');
         $searchInput->sendKeys(WebDriverKeys::ENTER);
+        $crawler = $client->waitFor('.g');
         $client->takeScreenshot('screen.png'); // Yeah, screenshot!
-        $this->assertEquals('test query - Google Search', $client->getTitle());
-        $this->assertCount(1, $crawler->filter('.g'));
+        $results = $crawler->filter('div.g');
+
+        $results->each(function (Crawler $node) {
+            $title = $node->filter('h3')->text();
+            $url = $node->filter('a')->attr('href');
+
+            echo $title . ": " . $url . "\n";
+        });
+        $this->assertEquals('marcus rashford - Szukaj w Google', $client->getTitle());
         $client->quit();
 
 
